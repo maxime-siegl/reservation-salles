36c78092532136acc33b44975c84f35f504a587f
louise-decombe <62133595+louise-decombe@users.noreply.github.com>
2020-06-11 18:28:10 +0200

    style + tableau fonctionnel

--- index.php
+++ index.php
@@ -2,11 +2,26 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Accueil</title>
 </head>
 <body>
-    <header></header>
-    <main></main>
+  <header>
+    <?php
+  // Si s'utilisateur est loggé le header est personnalisé
+  if (isset($_SESSION['login']))
+  {
+      echo '<ul> <li><a href="profil.php">   Tu es connecté(e)      ' . $_SESSION['login'] . '</a></li>' . '<li><a href="discussion.php"> accéder au chat </a></li>' . '<li><a href="profil.php?deconnexion"> deconnexion </a></li></ul>';
+  }
+  else
+  { ?>
+      <ul>
+      <li><a href="connexion.php">connexion</a></li>
+      <li><a href="inscription.php">inscription</a></li>
+      </ul>
+    <?php
+  } ?>
+
+  </header>    <main></main>
     <footer></footer>
 </body>
 </html>
